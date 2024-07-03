f19bee7c856a2505063b6e76da0b0cce18c14a30
mitj500 <mitjain9987900@gmail.com>
2024-07-03 22:57:58 +0530

    new  files

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const port = 3000
 
 app.use(express.static('public')) // Serve static files from the 'public' folder
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+
+app.get('/', (req, res) => {
+    res.sendFile()
     res.status(200)
   })
   app.get('/see', (req, res) => {
